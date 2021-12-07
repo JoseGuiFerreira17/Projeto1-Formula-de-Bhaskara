3184bf5331575bd8c6d51c3c02f19f77ac2ab75e
José Guilherme Mouta Ferreira <59844799+JoseGuiFerreira17@users.noreply.github.com>
2021-12-07 00:14:36 -0300

    Update CalcularBhaskara.py

--- CalcularBhaskara.py
+++ CalcularBhaskara.py
@@ -6,9 +6,7 @@ c = c1
 
 delta = (b*b) - 4*a*c
 print(delta)
-if delta < 0:
-    print("Impossivel calcular")
-elif(a == 0):
+if delta < 0 or a == 0:
     print("Impossivel calcular")
 else:
     x1 = (-b + math.pow(delta, 0.5)) / (2*a)
